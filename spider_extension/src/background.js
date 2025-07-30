4e38f8c47d6223f92342ec1572ec1ddce9229057
Daniel Kurz <danielkurz03@protonmail.com>
2025-07-30 19:12:16 +0200

    send message from service worker to content script

--- spider_extension/src/background.js
+++ spider_extension/src/background.js
@@ -1,3 +1,7 @@
 import { init } from "./spider_extension/background.mjs";
 
 init();
+
+chrome.tabs.onActivated.addListener((info) => {
+  chrome.tabs.sendMessage(info.tabId, { action: "doSomething" });
+});
--- spider_extension/src/content.js
+++ spider_extension/src/content.js
@@ -1,3 +1,7 @@
 import { init } from "./spider_extension/content.mjs";
 
 init();
+
+chrome.runtime.onMessage.addListener((request, _sender, sendResponse) => {
+  console.log({ action: request.action });
+});
